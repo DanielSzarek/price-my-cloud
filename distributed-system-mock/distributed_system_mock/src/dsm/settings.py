1fb0f6974469029ebeb60aee1d78ffd27bad4394
Daniel Szarek <daniel.szarek98@gmail.com>
2022-10-15 18:22:12 +0200

    Finish basic configuration of the project

--- distributed-system-mock/distributed_system_mock/src/dsm/settings.py
+++ distributed-system-mock/distributed_system_mock/src/dsm/settings.py
@@ -31,6 +31,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "django_extensions",
+    # local
+    "node",
 ]
 
 MIDDLEWARE = [
