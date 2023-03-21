fefbf99c25a2e56492dc85b2884cf9dc0d332e3b
Daniel Szarek <daniel.szarek98@gmail.com>
2023-03-21 20:55:20 +0100

    Change logging

--- micro-system-mock/micro_system_mock/main.py
+++ micro-system-mock/micro_system_mock/main.py
@@ -9,26 +9,27 @@ from .config import settings
 from .db import get_logs, create_log, LogCreate
 
 app = FastAPI()
-logger = logging.getLogger("uvicorn")
+logger = logging.getLogger(__name__)
 
 
 @app.get("/")
 def read_root():
     start = time()
+    logger.error("Start")
     if settings.SHOULD_PERFORM_CPU_OPERATIONS:
         utils.cpu_operations()
 
     if settings.SHOULD_CALL_APIS:
-        logger.info("Calling API")
+        logger.error("Calling API")
         utils.call_apis()
 
     if settings.SHOULD_CALL_DB:
-        logger.info("Calling DB")
+        logger.error("Calling DB")
         try:
             log = LogCreate(log_data="Log message")
             create_log(log)
             logs = get_logs()
-            logger.info(f"DB log Count: {len(logs)}")
+            logger.error(f"DB log Count: {len(logs)}")
         except Exception as e:
             logger.error(str(e))
 
