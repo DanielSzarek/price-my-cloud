d831924713c7346f15b6d91a75b06ab9e3f99207
Daniel Szarek <daniel.szarek98@gmail.com>
2023-03-21 22:37:56 +0100

    int casting

--- micro-system-mock/micro_system_mock/config.py
+++ micro-system-mock/micro_system_mock/config.py
@@ -7,8 +7,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     SHOULD_PERFORM_CPU_OPERATIONS = getenv("SHOULD_PERFORM_CPU_OPERATIONS", True)
     CPU_OPERATIONS = SimpleNamespace(
-        RANGE_FROM=getenv("RANGE_FROM", 500_000),
-        RANGE_TO=getenv("RANGE_TO", 10_000_000),
+        RANGE_FROM=int(getenv("RANGE_FROM", 500_000)),
+        RANGE_TO=int(getenv("RANGE_TO", 10_000_000)),
     )
     SHOULD_CALL_DB = bool(getenv("SHOULD_CALL_DB", False))
     DB_CONNECTION_STRING = getenv(
