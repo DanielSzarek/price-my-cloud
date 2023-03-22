338ef226f519603ea583f05f746ff6332dbec06c
Daniel Szarek <daniel.szarek98@gmail.com>
2023-03-22 17:40:46 +0100

    Fix

--- micro-system-mock/micro_system_mock/main.py
+++ micro-system-mock/micro_system_mock/main.py
@@ -25,7 +25,6 @@ def read_root():
 
     if settings.SHOULD_CALL_APIS:
         logger.error("Calling API")
-        global counter
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(utils.call_apis_async(counter))
